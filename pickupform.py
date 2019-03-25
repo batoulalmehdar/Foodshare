a2fa03f999694114852464900413f7ac9692fecb
kirstenwendell <44243612+kirstenwendell@users.noreply.github.com>
2019-03-25 22:34:32 +0800

    change form'

--- pickupform.py
+++ pickupform.py
@@ -20,10 +20,10 @@ def sign_up():
         "https://api.mailgun.net/v3/sandboxd21d655ac64e48d08c7c0e245e620ae0.mailgun.org/messages",
         auth=("api", "7df13535b905f6f58d8b1f9b3aae0874-acb0b40c-c59e0ed6"),
         data={"from": "Excited User <mailgun@sandboxd21d655ac64e48d08c7c0e245e620ae0.mailgun.org>",
-        "to": form_data["mail"],
+        "to": form_data["user_mail"],
                   "subject": "Pick Up Meals",
-                  "text": form_data["name", "mail", "number", "postcode", "address1", "address2", "address3", "town",
-                  "date", "meals", "comments"]})
+                  "text": form_data["user_name", "user_mail", "user_number", "postcode", "address1", "address2", "address3", "town",
+                  "user_date", "user_meals", "user_comments"]})
 
     return "Thank you and we will be picking up the meal/s shortly."
 app.run(debug=True)
