b176d6ae35616301843d014d3870add5b1cec0bc
kirstenwendell <44243612+kirstenwendell@users.noreply.github.com>
2019-03-25 21:58:58 +0800

    change the form names

--- pickupform.py
+++ pickupform.py
@@ -15,13 +15,14 @@ def sign_up():
     print(form_data)
 
     #Send the email
-    request.post(
+    requests.post(
         "https://api.mailgun.net/v3/sandboxd21d655ac64e48d08c7c0e245e620ae0.mailgun.org/messages",
         auth=("api", "7df13535b905f6f58d8b1f9b3aae0874-acb0b40c-c59e0ed6"),
         data={"from": "Excited User <mailgun@sandboxd21d655ac64e48d08c7c0e245e620ae0.mailgun.org>",
-        "to": form_data["recipient"],
-                  "subject": "Hello",
-                  "text": form_data["body"]})
+        "to": form_data["user_mail"],
+                  "subject": "Pick Up Meals",
+                  "text": form_data["user_name", "user_mail", "user_number", "postcode", "address1", "address2", "address3", "town",
+                  "user_date", "user_meals", "user_comments"]})
 
-    return "All OK"
+    return "Thank you and we will be picking up the meal/s shortly."
 app.run(debug=True)
